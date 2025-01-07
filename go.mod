26099ed4f016cd507afa2b01c921e677e462b4a1
yudhawahyup27 <wyudha104@gmail.com>
2025-01-07 11:57:30 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yudhawahyup27/api-islam
+
+go 1.23.4
+
+require github.com/go-chi/chi/v5 v5.2.0
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Routes
+
+
+	// Start server
+	log.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
